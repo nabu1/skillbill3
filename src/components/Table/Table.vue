b7b0b3b1fa9251a0c674aa82bf60b4c5d752767d
maciekkand <maciek.kand@gmail.com>
2019-02-25 18:47:57 +0100

    WiP Table - filtry

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -8,14 +8,15 @@ export default {
     headers: [
       { text: 'id', sortable: true, value: 'id' },
       { text: 'title', sortable: true, value: 'title' },
-      { text: 'Name', sortable: false, value: 'first' },
-      { text: 'City', sortable: true, value: 'city' },
+      { text: 'First', sortable: false, value: 'first' },
+      { text: 'Last', sortable: false, value: 'last' },
       { text: 'Skill 1', sortable: true, value: 'skill_1' },
       { text: 'Rank 1', sortable: false, value: 'rank_1' },
       { text: 'Skill 2', sortable: true, value: 'rank_2' },
       { text: 'Rank 2', sortable: false, value: 'rank_2' },
       { text: 'Skill 3', sortable: true, value: 'rank_3' },
       { text: 'Rank 3', sortable: false, value: 'rank_3' },
+      { text: 'City', sortable: true, value: 'city' },
       { text: 'Picture', sortable: true, value: 'picture' }
     ],
     devs: [],
@@ -112,9 +113,7 @@ export default {
 <style scoped>
 
   .filters {
-    /* background-color: #424242; */
-   /*  background-color: gray; */
-    transform: scale(0.55, 1);
+    transform: scale(0.75, 1);
     transform-origin: left;
   }
 

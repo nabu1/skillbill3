058ef52bfe8d3702bb0473e7cf2e152160aed3ce
maciekkand <maciek.kand@gmail.com>
2019-02-26 22:23:25 +0100

    Done - dane dobrze się ściągają

--- src/services/ajaxHelpers.js
+++ src/services/ajaxHelpers.js
@@ -2,13 +2,13 @@ import { LITERALS } from './constants'
 
 export default (context, selectedSkills) => {
   console.log('%c ajaxHelpers selectedSkills = ' + JSON.stringify(selectedSkills), 'color: white')
-
+  let query = ''
   const skill_1 = selectedSkills.skill_1
-  const rank_1 = selectedSkills.rank_1
+  const rank_1 = selectedSkills.rank_1 || 1
   const skill_2 = selectedSkills.skill_2
-  const rank_2 = selectedSkills.rank_2
+  const rank_2 = selectedSkills.rank_2 || 1
   const skill_3 = selectedSkills.skill_3
-  const rank_3 = selectedSkills.rank_3
+  const rank_3 = selectedSkills.rank_3 || 1
 
   console.log('%c skill_1 = ' + skill_1, 'color: white')
   console.log('%c rank_1 = ' + rank_1, 'color: white')
@@ -19,31 +19,21 @@ export default (context, selectedSkills) => {
   console.log('%c skill_3 = ' + skill_3, 'color: white')
   console.log('%c rank_3 = ' + rank_3, 'color: white')
 
-  // const query = 's={id:1}'
+  if (skill_1 && rank_1 && skill_2 && rank_2 && skill_3 && rank_3) {
+    console.log('3 skille')
+    query = 's={skill_1:1}&q={"skill_1":"' + skill_1 + '",$and:[{"rank_1":{$gte:' + rank_1 + '}},{$and:[{"skill_2":"' + skill_2 + '"},{$and:[{"rank_2":{$gte:' + rank_2 + '}},{$and:[{"skill_3":"' + skill_3 + '"},{$and:[{"rank_3":{$gte:' + rank_3 + '}}]}]}]}]}]}'
+  }
+  else if (skill_1 && rank_1 && skill_2 && rank_2) {
+    console.log('2 skille')
+    query = 's={skill_1:1}&q={"skill_1":"' + skill_1 + '",$and:[{"rank_1":{$gte:' + rank_1 + '}},{$and:[{"skill_2":"' + skill_2 + '"},{$and:[{"rank_2":{$gte:' + rank_2 + '}}]}]}]}'
+  }
+  else if (skill_1 && rank_1) {
+    console.log('1 skill')
+    query = 's={skill_1:1}&q={"skill_1":"' + skill_1 + '",$and:[{"rank_1":{$gte:' + rank_1 + '}}]}'
+  }
+  else alert ('Nie wpisałeś któregoś skilla lub ranka')
 
-  // const query = `q={"skill_1":"C",$and:[{"rank_1":{$gte:5}}]}`
-
-  // let query = `q={"skill_1":"C",$and:[{"rank_1":{$gte:0}},{$and:[{"skill_2":"Java"},{$and:[{"rank_2":{$gte:3}},{$and:[{"skill_3":"JS"},{$and:[{"rank_3":{$gte:0}}]}]}]}]}]}`
-
-  /// let query = 'q={"skill_1":"JS",$and:[{"rank_1":{$gte:1}},{$and:[{"skill_2":"Java"},{$and:[{"rank_2":{$gte:1}},{$and:[{"skill_3":"ObjC"},{$and:[{"rank_3":{$gte:1}}]}]}]}]}]}'
-
-  ///let query = 'q={"skill_1":' + `"JS"` + ',$and:[{"rank_1":{$gte:' + 1 + '}},{$and:[{"skill_2":"' + "Java" +'"},{$and:[{"rank_2":{$gte:1}},{$and:[{"skill_3":"ObjC"},{$and:[{"rank_3":{$gte:1}}]}]}]}]}]}'
-
-  ///let query = 'q={"skill_1":' + `"JS"` + ',$and:[{"rank_1":{$gte:' + 1 + '}},{$and:[{"skill_2":"' + "Java" + '"},{$and:[{"rank_2":{$gte:' + 1 + '}},{$and:[{"skill_3":"ObjC"},{$and:[{"rank_3":{$gte:1}}]}]}]}]}]}'
-
-  /// let query = 'q={"skill_1":"' + "JS" + '",$and:[{"rank_1":{$gte:' + 1 + '}},{$and:[{"skill_2":"' + "Java" + '"},{$and:[{"rank_2":{$gte:' + 1 + '}},{$and:[{"skill_3":"' + "ObjC" + '"},{$and:[{"rank_3":{$gte:' + 1 + '}}]}]}]}]}]}'
-
-  /// let query = 'q={"skill_1":"' + "JS" + '",$and:[{"rank_1":{$gte:' + 1 + '}},{$and:[{"skill_2":"' + "Java" + '"},{$and:[{"rank_2":{$gte:' + 1 + '}},{$and:[{"skill_3":"' + "ObjC" + '"},{$and:[{"rank_3":{$gte:' + 1 + '}}]}]}]}]}]}'
-
-  let query1 = 'q={"skill_1":"' + skill_1 + '",$and:[{"rank_1":{$gte:' + rank_1 + '}},{$and:[{"skill_2":"' + skill_2 + '"},{$and:[{"rank_2":{$gte:' + rank_2 + '}},{$and:[{"skill_3":"' + skill_3 + '"},{$and:[{"rank_3":{$gte:' + rank_3 + '}}]}]}]}]}]}'
-
-
-  let query2 = 'q={"skill_1":"' + skill_1 + '",$and:[{"rank_1":{$gte:' + rank_1 + '}},{$and:[{"skill_2":"' + skill_2 + '"},{$and:[{"rank_2":{$gte:' + rank_2 + '}}]}]}]}'
-
-
-
-
-  const urlString = LITERALS.PREFIX + query2 + LITERALS.SUFFIX
+  const urlString = LITERALS.PREFIX + query + LITERALS.SUFFIX
   console.log('%c urlString =  ' + urlString, 'color: lime')
 
   return urlString

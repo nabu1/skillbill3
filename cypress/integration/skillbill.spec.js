49b9ad734fe1adc37857b644828cc215aa2a4d0e
maciekkand <maciek.kand@gmail.com>
2019-03-06 18:49:28 +0100

    WiP - cyprys: editing / updating, selected devs

--- cypress/integration/skillbill.spec.js
+++ cypress/integration/skillbill.spec.js
@@ -3,7 +3,7 @@ import { tableCell } from './cyhelper'
 for (let i = 0; i < 1 ; i++) {
 
   describe('I. Documents filtering', () => {
-    it.only('1. .. using JS=4, Java=2 and C without rank specified, should get at least 3 devs' +
+    it.skip('1. .. using JS=4, Java=2 and C without rank specified, should get at least 3 devs' +
       'and pressing Clear button should clear all the select boxes', () => {
 
       cy.visit('http://localhost:8080')
@@ -64,11 +64,11 @@ for (let i = 0; i < 1 ; i++) {
     it.skip('1. ..  should immediately remove him from the view', () => {
 
       cy.visit('http://localhost:8080').wait(4000)
-      cy.get('tr:nth-child(1) > td.text-xs-left.body-2:nth-child(1)').should('have.text', '5')
+      cy.get('tr:nth-child(1) > td.text-xs-left.body-2:nth-child(1)').should('have.text', '7')
 
       // deleting row
       cy.get("tr:nth-child(1) > td.justify-center.layout.px-0 > i:nth-child(3)").click()
-      cy.get('tr:nth-child(1) > td.text-xs-left.body-2:nth-child(2)').should('not.have.text', '5')
+      cy.get('tr:nth-child(1) > td.text-xs-left.body-2:nth-child(2)').should('not.have.text', '7')
     })
   })
 
@@ -76,7 +76,8 @@ for (let i = 0; i < 1 ; i++) {
     it.skip('1. .. should immediately add him to the view (JS=5, Java=5, C=5)', () => {
       cy.visit('http://localhost:8080').wait(2000)
 
-      cy.get('#main > nav > div > div.layout.xs12.sm1.d-flex.ml-2 > button.mt-3.mr-0.v-btn.theme--dark.primary > div').click().wait(1000)
+      //cy.get('#main > nav > div > div.layout.xs12.sm1.d-flex.ml-2 > button.mt-3.mr-0.v-btn.theme--dark.primary > div').click().wait(1000)
+      cy.get("[data-test='btnNewDev']").click({ force: true })
       cy.get("[data-test='first']").type('Hans')
       cy.get("[data-test='last']").type('Klops')
 
@@ -88,17 +89,59 @@ for (let i = 0; i < 1 ; i++) {
       cy.get('#app > div:nth-child(6) > div > div > div:nth-child(1) > a > div > div').click({ force: true})
 
       cy.get('#app > div:nth-child(5) > div > div > div:nth-child(1) > a > div > div').click({ force: true})
-cy.pause()
+// cy.pause()
+      cy.get("[data-test='btnSave']").click({ force: true })
+      cy.get("[data-test='find']").type('klops')
+      cy.get("[data-test='btnFind']").click()
+      cy.get(tableCell(1, 2)).contains('Junior').should('exist')
+      cy.get(tableCell(1, 3)).contains('Hans').should('exist')
+      cy.get(tableCell(1, 4)).contains('Klops').should('exist')
+    })
+  })
+
+  describe('IV. Editing a dev', () => {
+    it.skip('1. .. should immediately show modified data of a dev', () => {
+      cy.visit('http://localhost:8080').wait(2000)
+
+      cy.get("#app > div.application--wrap > div > div.elevation-6 > div.v-table__overflow > table > tbody > tr:nth-child(1) > td.justify-center.layout.px-0 > i:nth-child(1)").click()
+
+      cy.get("[data-test='first']").clear().type('Miś')
+      cy.get("[data-test='last']").clear().type('Uszatek')
+      cy.get('#app > div:nth-child(11) > div > div > div:nth-child(5) > a > div > div').click({ force: true})  // Architect
+
+      cy.get("[data-test='email']").clear().type('Mis@Uszatek.pl')
       cy.get("[data-test='btnSave']").click({ force: true })
 
+      cy.get("[data-test='find']").type('Uszatek')
+      cy.get("[data-test='btnFind']").click()
 
+      cy.get(tableCell(1, 2)).contains('Architect').should('exist')
+      cy.get(tableCell(1, 3)).contains('Miś').should('exist')
+      cy.get(tableCell(1, 4)).contains('Uszatek').should('exist')
+
+    })
+  })
+
+  describe('V. Selected devs', () => {
+    it.only('1. .. should be visible after being doubleclicked and button Selected was clicked. \
+      After clicking Reset butto, previous table id shown', () => {
+        
+      cy.visit('http://localhost:8080').wait(2000)
+
+      cy.contains('Uszatek').dblclick()
+      cy.contains('Worsnup').dblclick()
+      cy.contains('Suddell').dblclick()
+
+      cy.get("[data-test='btnSelected']").click()
+
+      cy.contains('Uszatek').should('exist')
+      cy.contains('Worsnup').should('exist')
+      cy.contains('Suddell').should('exist')
+
+      cy.get("[data-test='btnReset']").click().wait(1000)
+      cy.get('table > tbody > tr').its('length').should('be.gte', 10)
     })
   })
 }
 
-
-// ('#app > div:nth-child(10) > div > div > div:nth-child(1) > a > div > div')
-
-// document.querySelector("#main > div > div:nth-child(4) > div > div > div.v-input__slot > div.v-select__slot > div.v-select__selections > input[type=text]")
-// document.querySelector("#app > div:nth-child(3) > div > div > div.v-card__text > div > div > div:nth-child(7) > div > div > div.v-input__slot > div.v-select__slot > div.v-select__selections > input[type=text]")
-
+// ("#app > div.application--wrap > div > div.elevation-6 > div.v-table__overflow > table > tbody > tr:nth-child(3)")
